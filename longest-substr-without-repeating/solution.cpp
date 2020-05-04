723a108457042941ba726b3e9391539a9bedef79
Austin, Matthew [ETHUS NON-J&J] <MAustin7@ITS.JNJ.com>
2020-05-04 14:06:19 -0500

    forgot to hit save before last commit

--- longest-substr-without-repeating/solution.cpp
+++ longest-substr-without-repeating/solution.cpp
@@ -12,26 +12,26 @@ public:
     	int longest = 0;
 
     	for (int i=0; i<strLen; i++) {
-    		//make a 90 element array to hold whether letters have been seen
-    		//91 elements to accomadate all charecter ascii charecters (32-122)
-    		int letters[91];
+    		//make a 95 element array to hold whether letters have been seen
+    		//95 elements to accomadate all ascii charecters (32-127)
+    		int letters[95];
     		//fill it with 0's
-    		fill(letters, letters+91, 0);
+    		fill(letters, letters+95, 0);
     		int currentLength;
     		for (int j=i; j<strLen; j++) {
     			char c = s.at(j);
     			//get the charecter to an int by casting, which gives the ascii code
-    			//subtract 32 to align it with our array
+    			//subtract 32 to align it with our array (all the ascii's <32 are control)
     			int ci = int(c) - 32;
     			printf ("i: %d   j: %d   c: %c   ci: %d\r\n",i,j,c,ci);
     			
     			//case 1: last letter in string
     			if (j == (strLen - 1)) {
     				cout << "end!\r\n";
-    				if (letters[ci] > 0) {
-    					currentLength = j - i;
-    				} else {
-    					currentLength = j - i + 1;
+    				//check if the last charecter is a duplicate or not
+    				currentLength = j - i;
+    				if (letters[ci] == 0) {
+    					currentLength += 1
     				}
     				if (currentLength > longest) {
     					longest = currentLength;
